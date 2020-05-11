9ec8a0ef6b3dd8b8ee479aa400966399fe607a56
BernardChoo <bernardchoowk@gmail.com>
2020-05-11 22:55:55 +0800

    1st Commit. Hello world 2020

--- AY2017-18/2018-04/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var Ballot = artifacts.require("./Ballot.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Ballot, ["Trump","Dump","Jump","Hump"]);
-};
